66c8cd33a5f51aa35952713934187ac11897d195
Jorge Bernuil <programacionjbs@gmail.com>
2023-10-10 20:35:33 -0500

    subiendo el proyectos

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="grid-index">
+        <header class="header">cabecera</header>
+        
+        <main class="main">cuerpo</main>
+
+        <footer class="footer">pie de pagina</footer>
+    </div>
+</body>
+</html>
